284961d1aac02c9048eee179d8ff2a283254fbf2
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-08-29 19:22:38 +0300

    26, 28,29, 34 ready

--- js/script34.js
+++ js/script34.js
@@ -1,35 +1,38 @@
+(function () {
+    'use strict'
 
-const str ='Hello world';
+    const str ='Hello world';
 
-const InsertWordInto  = (str) => {
+    const InsertWordInto  = (str) => {
 
-    let index = 0;
+        let index = 0;
 
-    return function (str2) {
-        const arrStr = str.split(' ');
-        if (index > arrStr.length) index = 0;
-        arrStr.splice(index, 0, str2);
-        index +=1;
-        let result = arrStr.join(' ');
-        return result;
+        return function (str2) {
+            const arrStr = str.split(' ');
+            if (index > arrStr.length) index = 0;
+            arrStr.splice(index, 0, str2);
+            index +=1;
+            let result = arrStr.join(' ');
+            return result;
+        }
+
+
     }
 
+    const newString = InsertWordInto(str);
 
-}
+    const result1= newString('Odessa1');
+    console.log(result1);
 
-const newString = InsertWordInto(str);
+    const result2 = newString('Odessa2');
+    console.log(result2);
 
-const result1= newString('Odessa1');
-console.log(result1);
+    const result3 = newString('Odessa3');
+    console.log(result3);
 
-const result2 = newString('Odessa2');
-console.log(result2);
+    const result4 = newString('Odessa4');
+    console.log(result4);
 
-const result3 = newString('Odessa3');
-console.log(result3);
-
-const result4 = newString('Odessa4');
-console.log(result4);
-
-const result5 = newString('Odessa5');
-console.log(result5);
+    const result5 = newString('Odessa5');
+    console.log(result5);
+})();
