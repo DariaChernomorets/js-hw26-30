04432a76d21a4b21d26a79ceb07747e818dda100
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-08-30 21:30:47 +0300

    26, 27, 28, 29, 30, 34 ready

--- js/script30.js
+++ js/script30.js
@@ -1,24 +1,36 @@
-'use strict'
+(function () {
 
-const counter = (num) => {
+    'use strict'
 
-        let counterObj = {
-            increase:function (){
-                this.value +=1;
-                return this;
-            },
-            decrease:function () {
-                this.value -=1;
-                return this;
-            },
+    const counter = function (num){
 
-            value: function () {
-                const value = this.value;
-                return this;
-            }
-        }
+        let value = 0;
 
-      return counterObj;
+        this.increase = function (){
+            return value+=1;
+        };
 
-}
+        this.decrease =function () {
+            return value -=1;
+        };
+
+        this.value = function (){
+            return value;
+        };
+    }
+
+    let counter1 = new counter();
+
+    let counter2 = new counter();
+
+    console.log(counter1.increase());
+    console.log(counter2.decrease())
+    console.log(counter2.decrease())
+    console.log(counter2.decrease())
+    console.log(counter2.value())
+    console.log(counter2.decrease())
+    console.log(counter1.increase());
+    console.log(counter1.value())
+})();
+
 
